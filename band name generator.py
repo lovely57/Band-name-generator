c89e7d5a75a78476bb81637a66c3fe50f3be01ec
Lovely Mangal <76448141+lovely57@users.noreply.github.com>
2021-05-23 10:30:18 +0530

    Add files via upload

---
+++ band name generator.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator.\n")
+city_name=input("what is the city name?\n")
+pet_name=input("what is your pet name\n")
+print("your band name is" +" "+city_name+" "+pet_name)
